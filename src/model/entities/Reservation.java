c52c4935652a549926d1cd5d8a83c55a65cb7af7
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2025-09-18 14:21:31 -0300

    A very ineffective way to handle errors

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,43 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy" );
+
+    private int roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    public Reservation() {
+
+    }
+
+    public Reservation( int roomNumber, Date checkin, Date checkout ) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+    
+    public long duration() {
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert( diff, TimeUnit.MILLISECONDS );
+    }
+
+    public void updateDates( Date checkin, Date checkout ) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString(){
+        return "Reservatio: "
+        + "Room " + roomNumber
+        + ", check-in: " + sdf.format( checkin )
+        + ", check-out: " + sdf.format( checkout )
+        + ", " + duration() + " nights.";
+    }
+}
